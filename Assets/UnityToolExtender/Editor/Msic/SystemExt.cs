02b084c80f61f838248475c09ac2ef7bf658c7df
wingcd <wingcd@foxmail.com>
2022-02-28 20:25:21 +0800

    优化系统执行函数

--- Assets/UnityToolExtender/Editor/Msic/SystemExt.cs
+++ Assets/UnityToolExtender/Editor/Msic/SystemExt.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -15,7 +15,28 @@ namespace Wing.Tools.Editor
         public static string Execute(this string command, string argument, string workingDir = null, bool noWindow = true, bool needWait = false)
         {
             command = command.Replace("{DATA_PATH}", Application.dataPath);
-            
+            argument = argument?.Replace("{DATA_PATH}", Application.dataPath);
+
+            var cmdext = Path.GetExtension(command);
+            if (string.IsNullOrEmpty(cmdext))
+            {
+                var exts = new string[0];
+#if UNITY_EDITOR_WIN
+                exts = new[] {".exe", ".bat"};
+#else
+                exts = new[] {"", ".sh"};
+#endif
+                foreach (var ext in exts)
+                {
+                    var f = command + ext;
+                    if (File.Exists(f))
+                    {
+                        command = f;
+                        break;
+                    }
+                }
+            }
+
             if (string.IsNullOrEmpty(command))
             {
                 Debug.LogError($"command can not be empty");
@@ -27,7 +48,7 @@ namespace Wing.Tools.Editor
             start.Arguments = argument;
             start.CreateNoWindow = noWindow;
             start.ErrorDialog = true;
-            start.UseShellExecute = false;
+            start.UseShellExecute = !noWindow;
 
             if (!string.IsNullOrEmpty(workingDir))
             {
@@ -40,21 +61,36 @@ namespace Wing.Tools.Editor
                 start.WorkingDirectory = Application.dataPath; // typeof(SystemExt).Assembly.Location + "/../../../";
             }
 
-            start.RedirectStandardOutput = true;
-            start.RedirectStandardError = true;
-            start.RedirectStandardInput = true;
-            start.StandardOutputEncoding = UTF8Encoding.UTF8;
-            start.StandardErrorEncoding = UTF8Encoding.UTF8;
+            start.RedirectStandardOutput = noWindow;
+            start.RedirectStandardError = noWindow;
+            start.RedirectStandardInput = noWindow;
+            if (noWindow)
+            {
+                start.StandardOutputEncoding = UTF8Encoding.UTF8;
+                start.StandardErrorEncoding = UTF8Encoding.UTF8;
+            }
 
             Process p = Process.Start(start);
             p.Exited += (sender, e) => { UnityEngine.Debug.Log("Execute " + command + " done!"); };
-            p.ErrorDataReceived += (sender, e) => { UnityEngine.Debug.LogError(e.Data); };
+            p.ErrorDataReceived += (sender, e) =>
+            {
+                UnityEngine.Debug.LogError(e.Data);
+            };
+            p.OutputDataReceived += (sender, args) =>
+            {
+                UnityEngine.Debug.Log(args.Data);
+            };
 
             if (needWait)
             {
+                p.WaitForExit();
+            }
+
+            if (noWindow)
+            {
                 var ret = p.StandardOutput.ReadToEnd();
                 UnityEngine.Debug.Log(ret);
-                return ret;
+                return "";
             }
 
             return "";
